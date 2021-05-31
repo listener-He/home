ee4a0dc2a9e9276d5724b006b67d13eee4286852
hehh <lingtinzhe@icloud.com>
2021-05-31 20:09:50 +0800

    更改bing背景 为 https://github.com/xCss/bing 提供的API

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,43 +77,52 @@ function description(element) {
 }
 
 function background(element) {
-    /**
-     * 获取Bing壁纸
-     * 原先 YQL 已经无法提供服务了
-     * 改用 JsonBird：https://bird.ioliu.cn/
-     *
-     */
-    var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
-    var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
-    var index = sessionStorage.getItem("index");
-    var $panel = $(element);
-    if (imgUrls == null) {
-        imgUrls = new Array();
-        index = 0;
-        $.get(url, function (result) {
-            images = result.images;
-            for (let i = 0; i < images.length; i++) {
-                const item = images[i];
-                imgUrls.push(item.url);
-            }
+    if (element != null) {
+        var $panel = $(element);
+        $panel.css("background", "url('https://bing.ioliu.cn/v1/rand') center center no-repeat #666");
+        $panel.css("background-size", "cover");
+    } else {
+        /**
+         * 获取Bing壁纸
+         * 原先 YQL 已经无法提供服务了
+         * 改用 JsonBird：https://bird.ioliu.cn/
+         *
+         */
+        var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
+        var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
+        var index = sessionStorage.getItem("index");
+
+        if (imgUrls == null) {
+            imgUrls = new Array();
+            index = 0;
+            $.get(url, function (result) {
+                images = result.images;
+                for (let i = 0; i < images.length; i++) {
+                    const item = images[i];
+                    imgUrls.push(item.url);
+                }
+                var imgUrl = imgUrls[index];
+                var url = "https://www.bing.com" + imgUrl;
+                $panel.css("background", "url('" + url + "') center center no-repeat #666");
+                $panel.css("background-size", "cover");
+                sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
+                sessionStorage.setItem("index", index);
+            });
+        } else {
+            if (index == 7)
+                index = 0;
+            else
+                index++;
             var imgUrl = imgUrls[index];
             var url = "https://www.bing.com" + imgUrl;
             $panel.css("background", "url('" + url + "') center center no-repeat #666");
             $panel.css("background-size", "cover");
-            sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
             sessionStorage.setItem("index", index);
-        });
-    } else {
-        if (index == 7)
-            index = 0;
-        else
-            index++;
-        var imgUrl = imgUrls[index];
-        var url = "https://www.bing.com" + imgUrl;
-        $panel.css("background", "url('" + url + "') center center no-repeat #666");
-        $panel.css("background-size", "cover");
-        sessionStorage.setItem("index", index);
+        }
     }
+
+
+
 }
 
 $(document).ready(function () {
